6c71def5f33c61a11ae09c06f71665741feee4df
andtheysay <andtheysay@protonmail.ch>
2024-08-16 13:00:58 -0500

    add sensors to init.py

--- src/airflow_pulsar_plugin/__init__.py
+++ src/airflow_pulsar_plugin/__init__.py
@@ -1,8 +1,10 @@
 from airflow.plugins_manager import AirflowPlugin
 from airflow_pulsar_plugin.hooks.pulsar_hook import PulsarHook
 from airflow_pulsar_plugin.operators.pulsar_operator import PulsarOperator
+from airflow_pulsar_plugin.sensors.pulsar_sensor import PulsarSensor
 
 class AirflowPulsarPlugin(AirflowPlugin):
     name = "pulsar_plugin"
     hooks = [PulsarHook]
     operators = [PulsarOperator]
+    sensors = [PulsarSensor]
